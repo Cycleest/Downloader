10ad1430ae0d3f1c62fb61eafeb3ada196e9aaf6
Cycleest <oosnuckoo@gmail.com>
2015-07-20 04:53:38 -0700

    no message

---
+++ app/src/main/java/cycleest/downloader/DownloaderFragment.java
@@ -0,0 +1,7 @@
+package cycleest.downloader;
+
+/**
+ * Created by mercury on 7/17/2015.
+ */
+public class DownloaderFragment {
+}
---
+++ app/src/main/java/cycleest/downloader/ImageLoader.java
@@ -0,0 +1,7 @@
+package cycleest.downloader;
+
+/**
+ * Created by mercury on 7/19/2015.
+ */
+public class ImageLoader {
+}
