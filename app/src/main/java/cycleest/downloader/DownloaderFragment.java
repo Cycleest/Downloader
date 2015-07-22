b1065d93d664700c53ceb1a034028b0a4b23110b
Cycleest <oosnuckoo@gmail.com>
2015-07-22 16:53:24 +0400

    fixes and attempt to increase readability

--- app/src/main/java/cycleest/downloader/DownloaderFragment.java
+++ app/src/main/java/cycleest/downloader/DownloaderFragment.java
@@ -22,6 +22,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.List;
@@ -34,15 +35,41 @@ public class DownloaderFragment extends Fragment implements LoaderManager.Loader
 
     private final static int LOADER_ID = 0;
 
+    private TextView statusLabel;
     private ProgressBar progressBar;
+    private ImageView imagePreview;
+    private Button button;
+
     private int currentState;
     private int currentProgress;
     private String imagePathInFilesystem;
 
-    private BroadcastReceiver receiver;
+    private BroadcastReceiver updateReceiver;
+    private BroadcastReceiver notificationsReceiver;
 
-    public static final String DOWNLOAD_PROGRESS_UPDATED = "cycleest.downloader.action.DOWNLOAD_PROGRESS_UPDATED";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setRetainInstance(true);
+        updateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                onProgressUpdate(intent);
+            }
+        };
+        notificationsReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                onNotificationReceive(intent);
+            }
+        };
+
+        File myDir = getActivity().getFilesDir();
+        imagePathInFilesystem = new File(myDir, "testimage.jpg").getPath();
+        Log.d("TAAG", "onCreate");
+        Log.d("TAAG", String.valueOf(currentState));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -58,80 +85,126 @@ public class DownloaderFragment extends Fragment implements LoaderManager.Loader
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
-        Button button = (Button) view.findViewById(R.id.button);
+        statusLabel = (TextView) view.findViewById(R.id.statusLabel);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        imagePreview = (ImageView) view.findViewById(R.id.imageView);
+        button = (Button) view.findViewById(R.id.button);
         button.setOnClickListener((View.OnClickListener) this);
-        //ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
-        TextView statusLabel = (TextView) view.findViewById(R.id.statusLabel);
-        switch (currentState) {
-            case STATE_IDLE:
-                progressBar.setVisibility(ProgressBar.INVISIBLE);
-                break;
-            case STATE_DOWNLOADING:
-                statusLabel.setText(getResources().getString(R.string.downloading));
-                progressBar.setVisibility(View.VISIBLE);
-                progressBar.setProgress(currentState);
-                button.setEnabled(false);
-                break;
-            case STATE_DOWNLOADED:
-                statusLabel.setText(getResources().getString(R.string.downloaded));
-                ((ImageView) view.findViewById(R.id.imageView)).setImageDrawable(Drawable.createFromPath(imagePathInFilesystem));
-                button.setText(getResources().getString(R.string.open));
-                break;
-        }
+
+        //initState(currentState);
+
         Log.d("TAAG", "onCreateView");
         Log.d("TAAG", String.valueOf(currentState));
-        if(getLoaderManager().getLoader(LOADER_ID) != null){
-            getLoaderManager().initLoader(LOADER_ID, null, this);
-        }
+
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        LoaderManager loaderManager = getLoaderManager();
+        Log.d("LoaderManager", String.valueOf(loaderManager.hashCode()));
+        loaderManager.restartLoader(LOADER_ID, null, this);
+        ImageLoader loader = (ImageLoader) loaderManager.getLoader(LOADER_ID);
+        /*if (loader == null) {
+            loaderManager.initLoader(LOADER_ID, null, this);
+            loader = (ImageLoader) loaderManager.getLoader(LOADER_ID);
+        }*/
+        Log.d("loaderIsNull", loader == null ? "null" : "not null");
+        if (loader != null) {
 
-        setRetainInstance(true);
-        receiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                onProgressUpdate(intent);
+            int loaderStatus = loader.getLoaderStatus();
+            Log.d("loaderStatus", String.valueOf(loaderStatus));
+            switch (loaderStatus) {
+                case ImageLoader.STATE_DOWNLOADING:
+                    initState(STATE_DOWNLOADING);
+                    currentProgress = loader.getCurrentProgress();
+                    if (currentProgress == ImageLoader.UNKNOWN_FILE_SIZE) {
+                        progressBar.setIndeterminate(true);
+                    } else {
+                        progressBar.setProgress(currentProgress);
+                    }
+                    break;
+                case ImageLoader.STATE_DOWNLOADED:
+                    initState(STATE_DOWNLOADED);
             }
-        };
-        currentState = STATE_IDLE;
-        currentProgress = 0;
-        File mydir = getActivity().getFilesDir();
-        imagePathInFilesystem = new File(mydir, "testimage.jpg").getPath();
-        Log.d("TAAG", "onCreate");
-        Log.d("TAAG", String.valueOf(currentState));
+
+        }
     }
 
-    private void onProgressUpdate(Intent intent) {
-        currentProgress = intent.getIntExtra("cycleest.downloader.progress_amount", 0);
+    private void initState(int state) {
+        currentState = state;
+        switch (state) {
+            case STATE_IDLE:
+                initIdleState();
+                break;
+            case STATE_DOWNLOADING:
+                initDownloadingState();
+                break;
+            case STATE_DOWNLOADED:
+                initDownloadedState();
+                break;
+        }
+    }
+
+    private void initIdleState() {
+        statusLabel.setText(getString(R.string.idle));
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
+        progressBar.setProgress(0);
+        button.setEnabled(true);
+        imagePreview.setImageDrawable(null);
+    }
+
+    private void initDownloadingState() {
+        statusLabel.setText(getString(R.string.downloading));
+        progressBar.setVisibility(ProgressBar.VISIBLE);
         progressBar.setProgress(currentProgress);
+        button.setEnabled(false);
+        imagePreview.setImageDrawable(null);
+    }
+
+    private void initDownloadedState() {
+        statusLabel.setText(getString(R.string.downloaded));
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
+        progressBar.setIndeterminate(false);
+        progressBar.setProgress(progressBar.getMax());
+        button.setText(getString(R.string.open));
+        button.setEnabled(true);
+        imagePreview.setImageDrawable(Drawable.createFromPath(imagePathInFilesystem));
+    }
+
+    private void onProgressUpdate(Intent notification) {
+        progressBar.setProgress(notification.getIntExtra("cycleest.downloader.progress_amount", 0));
+    }
+
+    private void onNotificationReceive(Intent notification) {
+        switch (notification.getAction()) {
+            case ImageLoader.UNABLE_TO_TRACK_PROGRESS:
+                progressBar.setIndeterminate(true);
+                currentProgress = ImageLoader.UNKNOWN_FILE_SIZE;
+                break;
+            case ImageLoader.FAILED_TO_DOWNLOAD:
+                initState(STATE_IDLE);
+                Toast.makeText(getActivity(), getString(R.string.failed_to_download), Toast.LENGTH_LONG).show();
+                initIdleState();
+        }
     }
 
 
     @Override
     public Loader onCreateLoader(int id, Bundle args) {
-
-        currentState = STATE_DOWNLOADING;
-        ((TextView) getView().findViewById(R.id.statusLabel)).setText(getResources().getString(R.string.downloading));
         Log.d("TAAG", "onCreateLoader");
         Log.d("TAAG", String.valueOf(currentState));
-        return new ImageLoader(getActivity());
+        //LoaderManager lm = getLoaderManager();
+        Loader loader;// = lm.getLoader(LOADER_ID);
+        //if(loader == null){
+            loader =  new ImageLoader(getActivity(), progressBar.getMax());
+        //}
+        return loader;
     }
 
     @Override
     public void onLoadFinished(Loader loader, Object data) {
-        ((TextView) getView().findViewById(R.id.statusLabel)).setText(getResources().getString(R.string.downloaded));
-        ((Button) getView().findViewById(R.id.button)).setText(getResources().getString(R.string.open));
-        getView().findViewById(R.id.button).setEnabled(true);
-        File mydir = getActivity().getFilesDir();
-        File fileWithinMyDir = new File(mydir, "testimage.jpg");
-        Log.d("filepath", fileWithinMyDir.getPath());
-        ((ImageView) getView().findViewById(R.id.imageView)).setImageDrawable(Drawable.createFromPath(fileWithinMyDir.getPath()));
-
-        currentState = STATE_DOWNLOADED;
+        initState(STATE_DOWNLOADED);
         Log.d("TAAG", "onLoadFinished");
         Log.d("TAAG", String.valueOf(currentState));
     }
@@ -144,7 +217,7 @@ public class DownloaderFragment extends Fragment implements LoaderManager.Loader
     @Override
     public void onClick(View v) {
 
-        if (getLoaderManager().getLoader(LOADER_ID) != null) {
+        if (((ImageLoader) getLoaderManager().getLoader(LOADER_ID)).getLoaderStatus() == ImageLoader.STATE_DOWNLOADED) {
             Intent intent = new Intent();
             intent.setAction(Intent.ACTION_PICK);
             intent.setType("image/*");
@@ -161,9 +234,9 @@ public class DownloaderFragment extends Fragment implements LoaderManager.Loader
             startActivity(intent);
         } else {
             getView().findViewById(R.id.button).setEnabled(false);
-            getLoaderManager().initLoader(LOADER_ID, null, this);
-            getLoaderManager().getLoader(LOADER_ID).forceLoad();
-            getView().findViewById(R.id.progressBar).setVisibility(ProgressBar.VISIBLE);
+                //getLoaderManager().initLoader(LOADER_ID, null, this);
+                getLoaderManager().getLoader(LOADER_ID).forceLoad();
+            initState(STATE_DOWNLOADING);
         }
         Log.d("TAAG", "onClick");
         Log.d("TAAG", String.valueOf(currentState));
@@ -171,14 +244,21 @@ public class DownloaderFragment extends Fragment implements LoaderManager.Loader
 
     @Override
     public void onResume() {
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(receiver,
-                new IntentFilter(DOWNLOAD_PROGRESS_UPDATED));
         super.onResume();
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(getActivity());
+        IntentFilter downloadProgress = new IntentFilter(ImageLoader.DOWNLOAD_PROGRESS_UPDATED);
+        IntentFilter notification = new IntentFilter();
+        notification.addAction(ImageLoader.UNABLE_TO_TRACK_PROGRESS);
+        notification.addAction(ImageLoader.FAILED_TO_DOWNLOAD);
+        localBroadcastManager.registerReceiver(updateReceiver, downloadProgress);
+        localBroadcastManager.registerReceiver(notificationsReceiver, notification);
     }
 
     @Override
     public void onPause() {
-        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(receiver);
         super.onPause();
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(getActivity());
+        localBroadcastManager.unregisterReceiver(updateReceiver);
+        localBroadcastManager.unregisterReceiver(notificationsReceiver);
     }
 }
--- app/src/main/java/cycleest/downloader/ImageLoader.java
+++ app/src/main/java/cycleest/downloader/ImageLoader.java
@@ -8,7 +8,7 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import java.io.BufferedInputStream;
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
@@ -17,9 +17,33 @@ import java.net.URLConnection;
 public class ImageLoader extends AsyncTaskLoader {
 
     public static final String DOWNLOAD_PROGRESS_UPDATED = "cycleest.downloader.action.DOWNLOAD_PROGRESS_UPDATED";
+    public static final String UNABLE_TO_TRACK_PROGRESS = "cycleest.downloader.action.UNABLE_TO_TRACK_PROGRESS";
+    public static final String FAILED_TO_DOWNLOAD = "cycleest.downloader.action.FAILED_TO_DOWNLOAD";
+    public static final int KILOBYTE = 1024;
+    public static final int UNKNOWN_FILE_SIZE = -1;
+    public final static int STATE_IDLE = 0;
+    public final static int STATE_DOWNLOADING = 1;
+    public final static int STATE_DOWNLOADED = 2;
 
-    public ImageLoader(Context context) {
+    private final int PROGRESS_CAPACITY;
+
+    private int currentProgress;
+    private int loaderStatus;
+
+    public ImageLoader(Context context, int progressCapacity) {
         super(context);
+        PROGRESS_CAPACITY = progressCapacity;
+        currentProgress = 0;
+        loaderStatus = STATE_IDLE;
+    }
+
+    public int getLoaderStatus() {
+        Log.d("getLoaderStatus", String.valueOf(loaderStatus));
+        return loaderStatus;
+    }
+
+    public int getCurrentProgress() {
+        return currentProgress;
     }
 
     @Override
@@ -29,37 +53,107 @@ public class ImageLoader extends AsyncTaskLoader {
             URLConnection connection = url.openConnection();
             connection.connect();
 
+            byte dataBuffer[];
+            int fileLength = getFileLengthAndBroadcastIfFailed(connection);
+            dataBuffer = initializeDataBufferAccordingTo(fileLength);
 
-            String filepath = super.getContext().getFilesDir().getPath();
-            Log.d("dir", Environment.getDataDirectory().getPath());
+            InputStream StreamFromURL = new BufferedInputStream(connection.getInputStream());
+            OutputStream StreamToFileSystem = super.getContext().openFileOutput("testimage.jpg", Context.MODE_PRIVATE);
 
-            InputStream input = new BufferedInputStream(connection.getInputStream());
-
-            OutputStream output = super.getContext().openFileOutput("testimage.jpg", Context.MODE_PRIVATE);
-
-            int fileLength = connection.getContentLength();
-
-            byte data[] = new byte[fileLength > 0 ? fileLength / 100 : 1024];
-            long total = 0;
-            int count;
-            Intent progressNotifier = new Intent();
-            progressNotifier.setAction(DOWNLOAD_PROGRESS_UPDATED);
-
-            LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(getContext());
-            while ((count = input.read(data)) != -1) {
-                total += count;
-                progressNotifier.putExtra("cycleest.downloader.progress_amount", (int) (total * 100 / fileLength));
-                broadcastManager.sendBroadcast(progressNotifier);
-                Thread.sleep(30);
-                output.write(data, 0, count);
+            if (isFileLengthRetrieveSucceed(fileLength)) {
+                startDownloadWithProgressTrack(StreamFromURL, StreamToFileSystem, dataBuffer, fileLength);
+            } else {
+                startDownloadWithoutProgressTrack(StreamFromURL, StreamToFileSystem, dataBuffer);
             }
-            output.flush();
-            output.close();
-            input.close();
+            StreamToFileSystem.flush();
+            StreamToFileSystem.close();
+            StreamFromURL.close();
+            loaderStatus = STATE_DOWNLOADED;
+
         } catch (Exception e) {
-            Log.e("happens", "smth");
+            sendBroadcastNotification(FAILED_TO_DOWNLOAD);
+            loaderStatus = STATE_IDLE;
             e.printStackTrace();
         }
         return null;
+    }
+
+    private byte[] initializeDataBufferAccordingTo(int fileLength) {
+        byte[] dataBuffer;
+        if (isFileLengthRetrieveSucceed(fileLength)) {
+            dataBuffer = getBufferRelativeToFileLength(fileLength);
+        } else {
+            dataBuffer = getDefaultSizeBuffer();
+        }
+        return dataBuffer;
+    }
+
+    private byte[] getBufferRelativeToFileLength(int fileLength) {
+        byte dataBuffer[];
+        if (fileLength <= PROGRESS_CAPACITY) {
+            dataBuffer = new byte[1];
+        } else {
+            dataBuffer = new byte[fileLength / PROGRESS_CAPACITY];
+        }
+        return dataBuffer;
+    }
+
+    private byte[] getDefaultSizeBuffer() {
+        return new byte[KILOBYTE];
+    }
+
+    private int getFileLengthAndBroadcastIfFailed(URLConnection connection) {
+        int fileLength = connection.getContentLength();
+        if (fileLength == UNKNOWN_FILE_SIZE) {
+            sendBroadcastNotification(UNABLE_TO_TRACK_PROGRESS);
+        }
+        return fileLength;
+    }
+
+    private boolean isFileLengthRetrieveSucceed(int fileLength) {
+        return fileLength != UNKNOWN_FILE_SIZE;
+    }
+
+    private void startDownloadWithProgressTrack(InputStream input, OutputStream output, byte[] dataBuffer, int fileLength) throws IOException {
+        long retrievedBytesCount = 0;
+        int inBufferBytesCount;
+        Intent progressNotifier = new Intent();
+        progressNotifier.setAction(DOWNLOAD_PROGRESS_UPDATED);
+
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(getContext());
+        loaderStatus = STATE_DOWNLOADING;
+        while ((inBufferBytesCount = input.read(dataBuffer)) != -1) {
+            retrievedBytesCount += inBufferBytesCount;
+            currentProgress = (int) (retrievedBytesCount * PROGRESS_CAPACITY / fileLength);
+            progressNotifier.putExtra("cycleest.downloader.progress_amount", currentProgress);
+            broadcastManager.sendBroadcast(progressNotifier);
+            output.write(dataBuffer, 0, inBufferBytesCount);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void startDownloadWithoutProgressTrack(InputStream input, OutputStream output, byte[] dataBuffer) throws IOException {
+        int inBufferBytesCount;
+        loaderStatus = STATE_DOWNLOADING;
+        currentProgress = UNKNOWN_FILE_SIZE;
+        while ((inBufferBytesCount = input.read(dataBuffer)) != -1) {
+            output.write(dataBuffer, 0, inBufferBytesCount);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendBroadcastNotification(String notification) {
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(getContext());
+        Intent notifier = new Intent();
+        notifier.setAction(notification);
+        broadcastManager.sendBroadcast(notifier);
     }
 }
